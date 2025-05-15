5cd873b3751e183057832b3491480c607e094b87
viana pedroso <arthurpedrosoprofissional>
2025-05-15 17:33:10 -0300

    finalizado exercicio 1

--- Exercicio01/src/exercicio01/Exercicio01.java
+++ Exercicio01/src/exercicio01/Exercicio01.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio01;
 
-/**
- *
- * @author pedro
- */
+import java.util.Scanner;
+
 public class Exercicio01 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static boolean numeroPerfeito(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int somaDivisores = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                somaDivisores += i;
+            }
+        }
+
+        return somaDivisores == n;
     }
-    
+
+    public static void main(String[] args) {
+        //Crie um método que receba um número inteiro e retorne true se for um número perfeito 
+        //(a soma de seus divisores próprios é igual ao número, ex: 6 = 1 + 2 + 3).
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um numero:");
+        int n = scan.nextInt();
+
+        System.out.println(numeroPerfeito(n));
+
+    }
+
 }
